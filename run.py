55dd8557ab09b95b32c3e0ec2613d2810efbea8d
RamyApepone apepone <raykelenda@yahoo.fr>
2024-09-11 18:07:45 +0000

    Add tic toc toe game structure

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# tic toc toe game 
+# setup board
+gameBoard = ["-", "-", "-",
+             "-", "-", "-",
+             "-", "-", "-"]
+cur_player = "@"
+player_winner = None
+game_running = True
+
+# This statement will print the game board
+# player input data
+# checking the game 
+# player turn
+# checking the game for win or tie
