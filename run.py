4a7bd36f70c05cf4ea3359882e15a43712edc54c
RamyApepone apepone <raykelenda@yahoo.fr>
2024-09-11 18:56:40 +0000

    Add check game horizontal

--- run.py
+++ run.py
@@ -6,6 +6,9 @@ gameBoard = ["-", "-", "-",
 cur_player = "@"
 player_winner = None
 game_running = True
+
+print("Welcome to RK tic tac toe game")
+
 # This statement will print the game board
 def myBoard(gameBoard):
   print(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2])
@@ -21,9 +24,22 @@ def myPlayerData(myBoard):
         myBoard[player_inp-1] = cur_player
     else:
         print("There is already a player on the spot!")
-# checking the game 
+
+# checking the game win or tie
+def checkGameHorizontal(myBoard):
+    global winningPlayer
+    if myBoard[0] == myBoard[1] == myBoard[2] and myBoard[1] != "-":
+        winningPlayer = myBoard[0]
+        return True
+    elif myBoard[3] == myBoard[4] == myBoard[5] and myBoard[3] != "-":
+        winningPlayer = myBoard[3]
+        return True
+    elif myBoard[6] == myBoard[7] == myBoard[8] and myBoard[6] != "-":
+        winningPlayer = myBoard[6]
+        return True
+
 # player turn
-# checking the game for win or tie
+# checking again game win or tie
 while game_running:
     myBoard(gameBoard)
     myPlayerData(gameBoard)
