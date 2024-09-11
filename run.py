e631c3ba79c4cfd589bb17412d0d4207d3c7ae53
RamyApepone apepone <raykelenda@yahoo.fr>
2024-09-11 19:37:59 +0000

    Add function to check if the game is tie

--- run.py
+++ run.py
@@ -18,39 +18,57 @@ def myBoard(gameBoard):
   print(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8])
 
 # player input data
-def myPlayerData(myBoard):
+def myPlayerData(gameBoard):
     player_inp = int(input("Please Enter number from 1 to 9: "))
-    if player_inp >= 1 and player_inp <= 9 and myBoard[player_inp-1] == "-":
-        myBoard[player_inp-1] = cur_player
+    if player_inp >= 1 and player_inp <= 9 and gameBoard[player_inp-1] == "-":
+        gameBoard[player_inp-1] = cur_player
     else:
-        print("There is already a player on the spot!")
+        print("Please wait for your turn!")
 
 # checking the game win or tie
-def checkGameHorizontal(myBoard):
+def checkGameHorizontal(gameBoard):
     global winningPlayer
-    if myBoard[0] == myBoard[1] == myBoard[2] and myBoard[1] != "-":
-        winningPlayer = myBoard[0]
+    if gameBoard[0] == gameBoard[1] == gameBoard[2] and gameBoard[1] != "-":
+        winningPlayer = gameBoard[0]
         return True
-    elif myBoard[3] == myBoard[4] == myBoard[5] and myBoard[3] != "-":
-        winningPlayer = myBoard[3]
+    elif gameBoard[3] == gameBoard[4] == gameBoard[5] and gameBoard[3] != "-":
+        winningPlayer = gameBoard[3]
         return True
-    elif myBoard[6] == myBoard[7] == myBoard[8] and myBoard[6] != "-":
-        winningPlayer = myBoard[6]
-        return True
-        
-def Check_the_row(myBoard):
-    global winningPlayer
-    if myBoard[0] == myBoard[3] == myBoard[6] and myBoard[0] != "-":
-        winningPlayer = myBoard[0]
-        return True
-    elif myBoard[1] == myBoard[4] == myBoard[7] and myBoard[1] != "-":
-        winningPlayer = myBoard[1]
-        return True
-    elif myBoard[2] == myBoard[5] == myBoard[8] and myBoard[2] != "-":
-        winningPlayer = myBoard[2]
+    elif gameBoard[6] == gameBoard[7] == gameBoard[8] and gameBoard[6] != "-":
+        winningPlayer = gameBoard[6]
         return True
 
-# player turn
+def Check_the_row(gameBoard):
+    global winningPlayer
+    if gameBoard[0] == gameBoard[3] == gameBoard[6] and gameBoard[0] != "-":
+        winningPlayer = gameBoard[0]
+        return True
+    elif gameBoard[1] == gameBoard[4] == gameBoard[7] and gameBoard[1] != "-":
+        winningPlayer = gameBoard[1]
+        return True
+    elif gameBoard[2] == gameBoard[5] == gameBoard[8] and gameBoard[2] != "-":
+        winningPlayer = gameBoard[2]
+        return True
+
+def check_my_diag(gameBoard):
+    global winningPlayer
+    if gameBoard[0] == gameBoard[4] == gameBoard[8] and gameBoard[0] != "-":
+        winningPlayer = gameBoard[0]
+        return True
+    elif gameBoard[2] == gameBoard[4] == gameBoard[6] and gameBoard[2] != "-":
+        winningPlayer = gameBoard[2]
+        return True
+
+def checktieGame(myBoard):
+    global game_running
+    if "-" not in gameBoard:
+        myBoard(gameBoard)
+        print("This is tie game")
+        game_running = False
+
+
+
+# change player
 # checking again game win or tie
 while game_running:
     myBoard(gameBoard)
